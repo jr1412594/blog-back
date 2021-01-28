a8fc0d5fc8c3d5958d0c6e20f8dcb2838093c5a3
jr1412594 <jr.medina1412@gmail.com>
2021-01-27 20:06:53 -0700

    fixed error with key

--- src/blog_api/views.py
+++ src/blog_api/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import IsAdminUser
 
 class PostList(generics.ListCreateAPIView):
+    # permission_classes = [IsAdminUser]
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
     
